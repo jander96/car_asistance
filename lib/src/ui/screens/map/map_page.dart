8e63bf12e879d9997dd21ec7ca6e99729387d1bf
Jander Laffita <janderdeveloper@gmail.com>
2023-05-03 10:40:13 -0400

    reformat a wigget mappage

--- lib/src/ui/screens/map/map_page.dart
+++ lib/src/ui/screens/map/map_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatefulWidget {
-
   const MapPage({super.key});
 
   @override
@@ -12,22 +11,20 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-   late List<Marker> markers;
+  late List<Marker> markers;
 
-  
   @override
   void initState() {
-    
     markers = [
-     
       Marker(
         anchorPos: AnchorPos.align(AnchorAlign.center),
         height: 30,
         width: 30,
         point: LatLng(53.3498, -6.2603),
-        builder: (ctx) => const Icon(size: 34.0 ,Icons.pin_drop), // Aqui es donde se modifica el tamaño del marker
+        builder: (ctx) => const Icon(
+            size: 34.0,
+            Icons.pin_drop), // Aqui es donde se modifica el tamaño del marker
       ),
-      
     ];
 
     super.initState();
@@ -35,47 +32,60 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    
-    
     return Scaffold(
       appBar: AppBar(title: Text("Map")),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(53.3498, -6.2603),
-          zoom: 9.2,
-        ),
-        children: [
-          TileLayer(
-            userAgentPackageName: "com.technicalassistance.car_assistance",
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          ),
-          MarkerClusterLayerWidget( // este me permite agrupar varios markers, aumenta la  eficiencia y belleza de los mismos
-            options: MarkerClusterLayerOptions(
-              maxClusterRadius: 45,
-              size: const Size(40, 40),
-              anchor: AnchorPos.align(AnchorAlign.center),
-              fitBoundsOptions: const FitBoundsOptions(
-                padding: EdgeInsets.all(50),
-                maxZoom: 15,
-              ),
-              markers: markers,
-              builder: (context, markers) {
-                return Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.red),
-                  child: Center(
-                    child: Text(
-                      markers.length.toString(),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      body: Stack(
+        children: [map()],
       ),
+    );
+  }
+
+  FlutterMap map() {
+    final tileLayer = TileLayer(
+          userAgentPackageName: "com.technicalassistance.car_assistance",
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        );
+    var markerClusterLayerOptions = MarkerClusterLayerOptions(
+            maxClusterRadius: 45,
+            size: const Size(40, 40),
+            anchor: AnchorPos.align(AnchorAlign.center),
+            fitBoundsOptions: const FitBoundsOptions(
+              padding: EdgeInsets.all(50),
+              maxZoom: 15,
+            ),
+            markers: markers,
+            builder: (context, markers) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), color: Colors.red),
+                child: Center(
+                  child: Text(
+                    markers.length.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+          );
+    
+    // este me permite agrupar varios markers, aumenta la  eficiencia y belleza de los mismos
+    var markerClusterLayerWidget = MarkerClusterLayerWidget(options: markerClusterLayerOptions);
+
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(53.3498, -6.2603),
+        zoom: 9.2,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: ' OpenStreetMap contributors',
+          onSourceTapped: () {},
+        )
+      ],
+      children: [
+        tileLayer,
+        markerClusterLayerWidget,
+      ],
     );
   }
 }
