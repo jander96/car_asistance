79536375d90a27dc9d0219af9f20584fc0b8acbf
Jander Laffita <janderdeveloper@gmail.com>
2023-06-03 23:47:31 -0400

    fix bug no favorite update

--- lib/src/ui/widgets/affiliate_grid_card.dart
+++ lib/src/ui/widgets/affiliate_grid_card.dart
@@ -5,7 +5,6 @@ import '../../domain/model/affiliate_model.dart';
 import 'bottom_sheet_view_model.dart';
 import 'custom_bottom_sheet.dart';
 
-
 class CustomCard extends StatelessWidget {
   final Affiliate affiliate;
 
@@ -37,22 +36,25 @@ class _CardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final state = context.watch<SheetViewModel>().state;
-    return Container(
-      width: 150,
-      decoration: BoxDecoration(
-          boxShadow: const [BoxShadow(blurRadius: 5, offset: Offset(0, 2))],
-          borderRadius: BorderRadius.circular(4),
-          color: colors.onPrimary),
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      child: Padding(
-        padding: const EdgeInsets.all(4),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //* Imagen
-            GestureDetector(
-              onTap: () => _openBottomSheet(context, affiliate),
-              child: SizedBox(
+    final viewModel = context.read<SheetViewModel>();
+    return GestureDetector(
+      onTap: () => _openBottomSheet(context, affiliate).then((_) {
+        viewModel.isFavorite(affiliate.id);
+      }),
+      child: Container(
+        width: 150,
+        decoration: BoxDecoration(
+            boxShadow: const [BoxShadow(blurRadius: 5, offset: Offset(0, 2))],
+            borderRadius: BorderRadius.circular(4),
+            color: colors.onPrimary),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //* Imagen
+              SizedBox(
                 width: 150,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(4),
@@ -65,60 +67,66 @@ class _CardView extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 5),
+              const SizedBox(height: 5),
 
-            //* Title
-            SizedBox(
-              width: 150,
-              child: Text(
-                affiliate.name,
-                maxLines: 2,
-                style: textStyles.bodyLarge,
+              //* Title
+              SizedBox(
+                width: 150,
+                child: Text(
+                  affiliate.name,
+                  maxLines: 2,
+                  style: textStyles.bodyLarge,
+                ),
               ),
-            ),
-            const Spacer(),
+              const Spacer(),
 
-            //* Address
+              //* Address
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: SizedBox(
-                  height: 48,
-                  child: Text(
-                    affiliate.address,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: textStyles.bodySmall,
-                  )),
-            ),
-            const Spacer(),
-
-            //* Rating
-            SizedBox(
-              width: 150,
-              child: Row(
-                children: [
-                  Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-                  const SizedBox(width: 3),
-                  Text(affiliate.rating.toStringAsFixed(1),
-                      style: textStyles.bodyMedium
-                          ?.copyWith(color: Colors.yellow.shade800)),
-                  const Spacer(),
-                    if(state.isFavorite) const Icon(Icons.favorite,color: Colors.red,),
-                    if(!state.isFavorite) const Icon(Icons.favorite_border)
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: SizedBox(
+                    height: 48,
+                    child: Text(
+                      affiliate.address,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: textStyles.bodySmall,
+                    )),
               ),
-            )
-          ],
+              const Spacer(),
+
+              //* Rating
+              SizedBox(
+                width: 150,
+                child: Row(
+                  children: [
+                    Icon(Icons.star_half_outlined,
+                        color: Colors.yellow.shade800),
+                    const SizedBox(width: 3),
+                    Text(affiliate.rating.toStringAsFixed(1),
+                        style: textStyles.bodyMedium
+                            ?.copyWith(color: Colors.yellow.shade800)),
+                    const Spacer(),
+                    if (state.isFavorite)
+                      const Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    if (!state.isFavorite) const Icon(Icons.favorite_border)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  _openBottomSheet(BuildContext context, Affiliate affiliate) {
-    showBottomSheet(
+  Future<void> _openBottomSheet(BuildContext context, Affiliate affiliate) {
+    return showModalBottomSheet(
+        isScrollControlled: true,
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
           topLeft: Radius.circular(15),
